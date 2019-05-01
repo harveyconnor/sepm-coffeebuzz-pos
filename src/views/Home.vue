49eade4df6d07435b8299cbfb68d58f307c46ada
Harvey Connor <harveyconnor97@gmail.com>
2019-05-01 20:47:11 +1000

    Add link to home to theme

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
-    <span>Test</span>
+    <h1>This is the theme for reference:</h1>
+    <a href="https://startbootstrap.com/templates/sb-admin/" target="_blank">https://startbootstrap.com/templates/sb-admin/</a>
   </div>
 </template>
