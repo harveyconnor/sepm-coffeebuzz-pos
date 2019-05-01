95ec965b030e65c78c64d3245815cf5b3bd1b6b3
Harvey Connor <harveyconnor97@gmail.com>
2019-05-01 20:11:30 +1000

    Add theme & vue apollo

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue';
 import router from './router';
 
 import 'startbootstrap-sb-admin/scss/sb-admin.scss';
+import { createProvider } from './vue-apollo';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  apolloProvider: createProvider(),
   render: h => h(App),
 }).$mount('#app');
