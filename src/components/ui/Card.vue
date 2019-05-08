05648274291adcddbca7fcd9c84af0ec11330af2
Harvey Connor <harveyconnor97@gmail.com>
2019-05-08 20:51:15 +1000

    Add custom card component and on click handle with props

---
+++ src/components/ui/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card card-clickable" @click="handleclick">
+    <div class="card-body">
+      <div class="mr-5"><slot></slot></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  props: [
+    'handleclick'
+  ]
+}
+</script>
--- src/views/Coffee.vue
+++ src/views/Coffee.vue
@@ -1,32 +1,45 @@
 <template>
   <div class="row">
     <div class="col-xl-3 col-sm-6 mb-3">
+      <Card :handleclick="() => openItem('Cappucino')">Cappucino</Card>
+    </div>
+    <div class="col-xl-3 col-sm-6 mb-3">
       <div class="card">
         <div class="card-body">
-          <div class="mr-5">Cappucino</div>
+          <Card :handleclick="() => openItem('Latte')">Latte</Card>
         </div>
       </div>
     </div>
     <div class="col-xl-3 col-sm-6 mb-3">
       <div class="card">
         <div class="card-body">
-          <div class="mr-5">Latte</div>
+          <Card :handleclick="() => openItem('Hot Chocolate')">Hot Chocolate</Card>
         </div>
       </div>
     </div>
     <div class="col-xl-3 col-sm-6 mb-3">
       <div class="card">
         <div class="card-body">
-          <div class="mr-5">Cappucino</div>
-        </div>
-      </div>
-    </div>
-    <div class="col-xl-3 col-sm-6 mb-3">
-      <div class="card">
-        <div class="card-body">
-          <div class="mr-5">Cappucino</div>
+          <Card :handleclick="() => openItem('Espresso')">Espresso</Card>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import Card from '@/components/ui/Card';
+
+export default {
+  name: 'coffee',
+  components: {
+    Card
+  },
+  methods: {
+    openItem: (item) => {
+      alert(item)
+    }
+  }
+}
+
+</script>
