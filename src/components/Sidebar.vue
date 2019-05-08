fa13efe9ac18e1616d4473f59e5da5cb31b0c3fe
Harvey Connor <harveyconnor97@gmail.com>
2019-05-08 19:48:06 +1000

    Add coffee & sandwiches pages

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,14 +2,26 @@
   <ul class="sidebar navbar-nav">
     <li class="nav-item">
       <router-link to="/" class="nav-link">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Dashboard</span>
+        <i class="fas fa-fw fa-home"></i>
+        <span>Home</span>
       </router-link>
     </li>
     <li class="nav-item">
-      <router-link to="/about" class="nav-link">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Dashboard</span>
+      <router-link to="/coffee" class="nav-link">
+        <i class="fas fa-fw fa-coffee"></i>
+        <span>Coffee</span>
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/sandwiches" class="nav-link">
+        <i class="fas fa-fw fa-hamburger"></i>
+        <span>Sandwiches</span>
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link to="/help" class="nav-link">
+        <i class="fas fa-fw fa-question"></i>
+        <span>Help</span>
       </router-link>
     </li>
   </ul>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Help from './views/Help.vue';
+import Coffee from './views/Coffee.vue';
+import Sandwiches from './views/Sandwiches.vue';
 
 Vue.use(Router);
 
@@ -14,12 +17,19 @@ export default new Router({
       component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: '/help',
+      name: 'help',
+      component: Help,
+    },
+    {
+      path: '/coffee',
+      name: 'coffee',
+      component: Coffee,
+    },
+    {
+      path: '/sandwiches',
+      name: 'sandwiches',
+      component: Sandwiches,
     },
   ],
 });
