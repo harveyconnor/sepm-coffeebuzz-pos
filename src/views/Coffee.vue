ac2e43eb90ca30aade76156e1fe6658dadfee44a
Harvey Connor <harveyconnor97@gmail.com>
2019-05-08 20:26:52 +1000

    Add demo coffee items

--- src/views/Coffee.vue
+++ src/views/Coffee.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    <span>Coffee</span>
+  <div class="row">
+    <div class="col-xl-3 col-sm-6 mb-3">
+      <div class="card">
+        <div class="card-body">
+          <div class="mr-5">Cappucino</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-sm-6 mb-3">
+      <div class="card">
+        <div class="card-body">
+          <div class="mr-5">Latte</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-sm-6 mb-3">
+      <div class="card">
+        <div class="card-body">
+          <div class="mr-5">Cappucino</div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-3 col-sm-6 mb-3">
+      <div class="card">
+        <div class="card-body">
+          <div class="mr-5">Cappucino</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
