558c17bc56d263b2e22eae0bd45c792307bb1aa6
Hugh Castletine-Jackson <s3378114@student.rmit.edu.au>
2019-05-21 10:09:45 +1000

    Minor menu improvements.  Doesn't post to cart yet

--- src/views/Coffee.vue
+++ src/views/Coffee.vue
@@ -2,21 +2,27 @@
   <Layout>
     <div class="row">
       <div class="col-xl-3 col-sm-6 mb-3">
+        <img class="card-img-top" src="@/assets/Cappucino.jpg" alt="Card image">
         <Card :handleclick="() => openItem('Cappucino')">Cappucino</Card>
       </div>
       <div class="col-xl-3 col-sm-6 mb-3">
+        <img class="card-img-top" src="@/assets/Latte.jpg" alt="Card image">
         <Card :handleclick="() => openItem('Latte')">Latte</Card>
       </div>
       <div class="col-xl-3 col-sm-6 mb-3">
+        <img class="card-img-top" src="@/assets/HC.jpg" alt="Card image">
         <Card :handleclick="() => openItem('Hot Chocolate')">Hot Chocolate</Card>
       </div>
       <div class="col-xl-3 col-sm-6 mb-3">
+        <img class="card-img-top" src="@/assets/short-black.jpg" alt="Card image">
         <Card :handleclick="() => openItem('Espresso')">Espresso</Card>
       </div>
     </div>
     <Modal>
       <template v-slot:title>{{item}}</template>
-      <template v-slot:body>Hehhe</template>
+      <template v-slot:body>Quantity
+       <input type="number" value="1" min="1" max="20" step="1"/>
+      </template>
       <template v-slot:footer>
         <div class="col-4">
           <button type="button" class="btn btn-danger btn-block" data-dismiss="modal">Cancel</button>
